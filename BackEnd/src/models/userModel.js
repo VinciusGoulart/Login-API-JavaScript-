d78b375625200b87644b2266c91b1cd798a926ff
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-11-29 18:28:16 -0300

    Created telefone relationship with usuario

--- BackEnd/src/models/userModel.js
+++ BackEnd/src/models/userModel.js
@@ -8,16 +8,25 @@ const getAll = async () => {
 const signUp = async (userData) => {
     const { nome, email, senha, telefones } = userData;
 
-    const dateUTC = new Date(Date.now()).toUTCString(); 
+    const dateUTC = new Date(Date.now()).toUTCString();
 
-    const query = 'INSERT INTO userData(nome, email, senha, telefones, data_criacao, data_atualizacao, ultimo_login, token) VALUES (?,?,?,?,?,?,?,?)';
+    const query = 'INSERT INTO usuario(nome, email, senha, data_criacao, data_atualizacao, ultimo_login, token) VALUES (?,?,?,?,?,?,?)';
 
-    const [created] = await connection.execute(query,
-        [nome, email, senha, telefones, dateUTC, dateUTC, dateUTC, 'token']); // Correção nos placeholders
+    const [created] = await connection.execute(query,[nome, email, senha, dateUTC, dateUTC, dateUTC, 'token']);
+    //connection.execute(query,[nome, email, senha, dateUTC, dateUTC, dateUTC, 'token']).then(response => {
+    //    console.log(response);
+    //})
+
+    telefones.forEach(async element => {
+        const telQuery = 'INSERT INTO telefone(numero, ddd, usuario_id) VALUES (?,?,?)';     
+    
+        await connection.execute(telQuery,[element.numero, element.ddd, created.insertId]);
+    });
+    
 
     return {
         id: created.insertId,
-        data_criacao: dateUTC, 
+        data_criacao: dateUTC,
         data_atualizacao: dateUTC,
         ultimo_login: dateUTC,
         token: 'token'
